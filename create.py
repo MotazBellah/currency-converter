6b185deb9c27b5477a5cc4bd4f1c4146eff3e8c7
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-10-12 13:30:02 +0200

    Created currency DB and load the scraped data into it

--- create.py
+++ create.py
@@ -1,6 +1,14 @@
 import bs4 as bs
 from urllib2 import urlopen
+from flask import Flask
 from models import *
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///currency.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db.init_app(app)
 
 def scrap():
     source = urlopen('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml').read()
@@ -15,3 +23,13 @@ def scrap():
             newCurrency = CurrencyRate(time=time, currency=i['currency'], rate=i['rate'])
             db.session.add(newCurrency)
             db.session.commit()
+
+
+def main():
+    db.create_all()
+    scrap()
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
