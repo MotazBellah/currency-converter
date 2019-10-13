495bca8344dfbbf4e0283c399950c8c99a31d622
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-10-13 13:15:25 +0200

    Use SelectField in the form instead of stringField to make it easier for the user to select the currency type

--- wtform_fields.py
+++ wtform_fields.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
 from models import *
-from wtforms import StringField, FloatField
+from wtforms import StringField, FloatField, SelectField
 from wtforms.validators import InputRequired, Length
 
-
+CURRENCY_TYPE = [("EUR","EUR"),("USD","USD"),("JPY","JPY"),("BGN","BGN"),("CZK","CZK"),
+                ("GBP","GBP"),("HUF","HUF"),("PLN","PLN"),("RON","RON"),("SEK","SEK"),
+                ("CHF","CHF"),("ISK","ISK"),("NOK","NOK"),("HRK","HRK"),("RUB","RUB"),
+                ("TRY","TRY"),("AUD","AUD"),("BRL","BRL"),("CAD","CAD"),("CNY","CNY"),
+                ("HKD","HKD"),("IDR","IDR"),("ILS","ILS"),("INR","INR"),("KRW","KRW"),
+                ("MXN","MXN"),("MYR","MYR"),("NZD","NZD"),("PHP","PHP"),("SGD","SGD"),
+                ("THB","THB"),("ZAR","ZAR"),("DKK","DKK")]
+                
 class CurrencyCovert(FlaskForm):
     """CurrencyCovert form"""
-    src_currency = StringField('source currency', validators=[InputRequired(message="source currency required"), Length(min=3, max=3, message='currency must be between 3 charachters')])
-    dest_currency = StringField('destination currency', validators=[InputRequired(message="destination currency required"), Length(min=3, max=3, message='currency must be between 3 charachters')])
+    # src_currency = StringField('source currency', validators=[InputRequired(message="source currency required"), Length(min=3, max=3, message='currency must be between 3 charachters')])
+    src_currency = SelectField('source currency', choices=CURRENCY_TYPE, validators=[InputRequired(message="source currency required")])
+    dest_currency = SelectField('destination currency',  choices=CURRENCY_TYPE, validators=[InputRequired(message="source currency required")])
     amount = FloatField('amount', validators=[InputRequired(message="amount required")])
     date = StringField('reference date', validators=[InputRequired(message="reference date required")])
