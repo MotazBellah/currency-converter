0a234d767d5bfd5cb52cdc1f0ff2bdeeb90728a8
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-10-13 12:51:03 +0200

    Created the convert route that convert the currency and return json format

--- app.py
+++ app.py
@@ -25,13 +25,32 @@ def index():
         dest_currency = conv_form.dest_currency.data
         amount = conv_form.amount.data
         date = conv_form.date.data
-        return redirect(url_for('convert'))
+        return redirect(url_for('convert', src_currency=src_currency, dest_currency=dest_currency, amount=amount, date=date))
 
     return render_template('index.html', form=conv_form)
 
-@app.route('/convert')
-def convert():
-    return 'hi'
+@app.route('/convert/<src_currency>/<dest_currency>/<float:amount>/<date>')
+def convert(src_currency, dest_currency, amount, date):
+    data = {}
+    if src_currency.lower() == 'eur':
+        rate = CurrencyRate.query.filter_by(currency=dest_currency.upper()).filter_by(time=date).first()
+        data['amount'] = rate.rate * amount
+        data['currency'] = dest_currency.upper()
+        print(rate.rate)
+    elif dest_currency.lower() == 'eur':
+        rate = CurrencyRate.query.filter_by(currency=src_currency.upper()).filter_by(time=date).first()
+        data['amount'] = (1 / rate.rate) * amount
+        data['currency'] = dest_currency.upper()
+    else:
+        des_rate = CurrencyRate.query.filter_by(currency=dest_currency.upper()).filter_by(time=date).first()
+        src_rate = CurrencyRate.query.filter_by(currency=src_currency.upper()).filter_by(time=date).first()
+        rate = des_rate.rate / src_rate.rate
+        data['amount'] = rate * amount
+        data['currency'] = dest_currency.upper()
+
+
+    # sorc_curr = CurrencyRate.query.filter_by(currency=src_currency)
+    return jsonify(data)
 
 
 if __name__ == '__main__':
